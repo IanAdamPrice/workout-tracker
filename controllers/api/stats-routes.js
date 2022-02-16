c13668d879341205cb6d9abf6408bcfb49622079
IanAdamPrice <ianadamprice@gmail.com>
2022-02-15 19:24:05 -0500

    cont

--- controllers/api/stats-routes.js
+++ controllers/api/stats-routes.js
@@ -1,3 +1,19 @@
 const router = require('express').Router();
-const { Stats } = require('../../models/Stats');
+const { Stats } = require('../../models');
 const withAuth = require('../../utils/auth');
+
+router.get('/', (req, res => {
+  Stats.findAll()
+    .then(dbStatsData => res.json(dbStatsData))
+    .catch(err => {
+      console.log(err);
+      res.statusCode(500).json(err);
+    });
+}));
+
+router.post('/', withAuth, (req,res) => {
+  Stats.create({
+    user_id: req.session.user_id,
+    
+  })
+})
