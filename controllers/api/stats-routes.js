cda68bf43d31ee2a68038fe660d5fda826c4d93d
IanAdamPrice <ianadamprice@gmail.com>
2022-02-15 19:43:38 -0500

    push to main branch

--- controllers/api/stats-routes.js
+++ controllers/api/stats-routes.js
@@ -14,6 +14,18 @@ router.get('/', (req, res => {
 router.post('/', withAuth, (req,res) => {
   Stats.create({
     user_id: req.session.user_id,
-    
+    height: req.body.height,
+    age: req.body.age,
+    weight: req.body.weight,
+    workout_count: req.body.workout_count,
+    meal_count: req.body.meal_count,
+    goals: req.body.goals
   })
-})
+  .then(dbStatsData => res.json(dbStatsData))
+  .catch(err => {
+    console.log(err);
+    res.status(400).json(err);
+  });
+});
+
+module.export = router;
---
+++ seeds/stats-seeds.js
@@ -0,0 +1,9 @@
+const { Stats } = require('../models');
+
+const statsdata = [
+  {
+    user_id: 1,
+    height: 5.7,
+    age: 28
+  }
+]
